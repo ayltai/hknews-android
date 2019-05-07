1c04b5ce1757b8f93dff46b15132df6bf855ca7a
Alan Tai <alan.tai@zuhlke.com>
2019-05-07 16:28:11 +0800

    Add dummy unit test for coverage generation

---
+++ app/src/test/java/com/github/ayltai/hknews/DummyTest.java
@@ -0,0 +1,11 @@
+package com.github.ayltai.hknews;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public final class DummyTest extends UnitTest {
+    @Test
+    public void test() {
+        Assert.assertEquals(1, Integer.parseInt("1"));
+    }
+}
