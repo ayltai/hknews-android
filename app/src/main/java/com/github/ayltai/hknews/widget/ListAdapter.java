f1205e36774f081fe44891f906e13816a93a016a
Alan Tai <alan.tai@zuhlke.com>
2019-06-27 11:50:30 +0800

    Remove featured news

--- app/src/main/java/com/github/ayltai/hknews/widget/ListAdapter.java
+++ app/src/main/java/com/github/ayltai/hknews/widget/ListAdapter.java
@@ -7,21 +7,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.github.ayltai.hknews.view.FeaturedItemPresenter;
-import com.github.ayltai.hknews.view.FeaturedListPresenter;
 import com.github.ayltai.hknews.view.ItemPresenter;
 import com.github.ayltai.hknews.view.ListPresenter;
 import com.github.ayltai.hknews.view.ModelPresenter;
 import com.github.ayltai.hknews.view.Presenter;
 
 public final class ListAdapter extends RecyclerView.Adapter<ItemViewHolder<Presenter, BaseView>> {
-    //region Constants
-
-    private static final int TYPE_DEFAULT  = 0;
-    private static final int TYPE_FEATURED = 1;
-
-    //endregion
-
     private final ListPresenter presenter;
     private final boolean       isCompactStyle;
 
@@ -37,26 +28,10 @@ public final class ListAdapter extends RecyclerView.Adapter<ItemViewHolder<Prese
         return this.presenter.getModel().isEmpty() ? 0 : 1 + this.presenter.getModel().size();
     }
 
-    @Override
-    public int getItemViewType(final int position) {
-        return position == 0 ? ListAdapter.TYPE_FEATURED : ListAdapter.TYPE_DEFAULT;
-    }
-
     @Nonnull
     @NonNull
     @Override
     public ItemViewHolder<Presenter, BaseView> onCreateViewHolder(@Nonnull @NonNull @lombok.NonNull final ViewGroup parent, final int viewType) {
-        if (viewType == ListAdapter.TYPE_FEATURED) {
-            if (holder == null) {
-                final FeaturedListPresenter presenter = new FeaturedListPresenter();
-                presenter.setModel(this.presenter.getModel());
-
-                holder = new ItemViewHolder<>(presenter, new FeaturedListView(parent.getContext()));
-            }
-
-            return holder;
-        }
-
         if (this.isCompactStyle) return new ItemViewHolder<>(new ItemPresenter<>(), new CompactItemView(parent.getContext()));
 
         return new ItemViewHolder<>(new ItemPresenter<>(), new CozyItemView(parent.getContext()));
@@ -64,14 +39,14 @@ public final class ListAdapter extends RecyclerView.Adapter<ItemViewHolder<Prese
 
     @Override
     public void onBindViewHolder(@Nonnull @NonNull @lombok.NonNull final ItemViewHolder<Presenter, BaseView> holder, final int position) {
-        if (position > 0 && holder.presenter instanceof ModelPresenter)  ((ModelPresenter)holder.presenter).setModel(this.presenter.getModel().get(position - 1));
+        if (holder.presenter instanceof ModelPresenter)  ((ModelPresenter)holder.presenter).setModel(this.presenter.getModel().get(position));
     }
 
     @Override
     public void onViewAttachedToWindow(@Nonnull @NonNull @lombok.NonNull final ItemViewHolder<Presenter, BaseView> holder) {
         holder.presenter.onViewAttached(holder.view);
 
-        if (holder.presenter instanceof ModelPresenter && !(holder.presenter instanceof FeaturedItemPresenter)) ((ModelPresenter)holder.presenter).bindModel();
+        if (holder.presenter instanceof ModelPresenter) ((ModelPresenter)holder.presenter).bindModel();
     }
 
     @Override
